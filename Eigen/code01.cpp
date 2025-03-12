36a1cb2e1c43c32e1af344760a3a5665d194e69b
TaoTao <45528733+huzixuan1@users.noreply.github.com>
2025-03-12 22:42:45 +0800

    Update code01.cpp

--- Eigen/code01.cpp
+++ Eigen/code01.cpp
@@ -1,1 +1,14 @@
 
+#include <iostream>
+#include "Eigen/Dense"
+
+int matrix01()
+{
+    Eigen::Matrix3d mat;
+    mat<<1,2,3,4,5,6,7,8,9;
+    std::cout<<"Matrix:"<<std::endl;
+    std::cout<<mat<<std::endl;
+
+    return 0;
+}
+
